e9b64081b2dd166957bb38409431d71fc28edbe3
hed <193773302@qq.come>
2024-05-29 14:39:29 +0800

    Initial commit

---
+++ src/main/java/com/example/easyexceldemon/EasyExcelDemonApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyexceldemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExcelDemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyExcelDemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyexceldemon/EasyExcelDemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyexceldemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyExcelDemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
